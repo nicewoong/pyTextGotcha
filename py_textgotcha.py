3cb3223bf5c1185e5dc7ee6849fc7c3637f93436
nicewoong <thoughtscode@gmail.com>
2018-01-09 10:54:17 +0900

    Add get_closing(), show_window(), save_image()

--- py_textgotcha.py
+++ py_textgotcha.py
@@ -6,7 +6,7 @@ And processes the image to extract the text portions using OpenCV-Python and CNN
 1) 입력 이미지 Gray Scale 적용 (OK)
     -
 2) Gradient 추출 (추후)
-3) Adaptive Threshold 적용한 image 반환, params 로 조정 값 받을 수 있도록
+3) Adaptive Threshold 적용한 image 반환, params 로 조정 값 받을 수 있도록 (OK)
 4) Close 적용한 image 반환
 5) Long Line remove 적용한 image 반환 (추후)
 6) 위 단계를 모두 거친 image 로부터 Contour 추출해서 Contours 반환
@@ -42,6 +42,13 @@ def get_gray(image_origin):
     return image_copy_grey
 
 
+def get_gray(file_path):
+    """ image file 을 읽어들여서 Gray-scale 을 적용한 이미지 객체를 반환합니다.
+    """
+    image_grey = cv2.imread(file_path, cv2.COLOR_BGR2GRAY)  # grey scale 로 복사합니다.
+    return image_grey
+
+
 def get_adaptive_gaussian_threshold(image_gray, block_size=15, subtract_val=2):
     """ Gray-scale 이 적용된 이미지를 입력받아서 Adaptive Threshold 를 적용한 흑백(Binary) 이미지객체를 반환합니다.
     @Params
@@ -49,24 +56,48 @@ def get_adaptive_gaussian_threshold(image_gray, block_size=15, subtract_val=2):
     * block_size : 픽셀에 적용할 threshold 값을 계산하기 위한 블럭 크기(홀수). 적용될 픽셀이 블럭의 중심이 됨.
     * subtract_val : 보정 상수
     """
-    adaptive_gaussian_image = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    image_adaptive_gaussian = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                                     cv2.THRESH_BINARY, block_size, subtract_val)
-    return adaptive_gaussian_image
+    return image_adaptive_gaussian  # todo 함수 안에서 생성한 객체 리턴하면 어떻게 되는거지? 주소값이 리턴되는 건가?? 소멸되지 않고?
 
 
 def get_adaptive_mean_threshold(image_gray, block_size=15, subtract_val=2):
     """ Gray-scale 이 적용된 이미지를 입력받아서 Adaptive Threshold 를 적용한 흑백(Binary) 이미지객체를 반환합니다.
     @Params
     * image_gray : Gray-scale 이 적용된 이미지객체
-    * block_size : 픽셀에 적용할 threshold 값을 계산하기 위한 블럭 크기(홀수). 적용될 픽셀이 블럭의 중심이 됨.
+    * block_size : 픽셀에 적용할   threshold 값을 계산하기 위한 블럭 크기(홀수). 적용될 픽셀이 블럭의 중심이 됨.
     * subtract_val : 보정 상수
     """
-    adaptive_mean_image = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
+    image_adaptive_mean = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,
                                                 block_size, subtract_val)
-    return adaptive_mean_image
+    return image_adaptive_mean
 
 
+def get_closing(image_gray, kernel_size_row, kernel_size_col):
+    """ Gray scale 이 적용된 이미지에 Morph Close 를 적용합니다.
+    Closing : dilation 수행을 한 후 바로 erosion 수행을 하여 본래 이미지 크기로
+    커널은 Image Transformation 을 결정하는 구조화된 요소
+    커널의 크기가 크거나, 반복횟수가 많아지면 과하게 적용되어 경계가 없어질 수도 있다
+    """
+    # make kernel matrix for dilation and erosion (Use Numpy)
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size_row, kernel_size_col))
 
+    # closing 적용
+    image_close = cv2.morphologyEx(image_gray, cv2.MORPH_CLOSE, kernel)
+    return image_close
+
+
+def open_window(image, title):
+    """ 윈도우를 열어서 이미지를 보여줍니다.
+    """
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)  # 사용자가 크기 조절할 수 있는 윈도우 생성
+    cv2.imshow(title, image)
+
+
+def save_image(image, file_path):
+    """ 이미지를 file 로 저장합니다.
+    """
+    cv2.imwrite(file_path, image)
 
 
 def main():
