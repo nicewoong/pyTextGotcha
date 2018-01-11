b4ec9f3ea14756fcd6250a89a91a23385875411d
nicewoong <thoughtscode@gmail.com>
2018-01-11 15:53:43 +0900

    Apply each function tu use the variables in the yaml configs file

--- pre_process.py
+++ pre_process.py
@@ -67,34 +67,42 @@ def get_gray(image_origin):
     return image_copy_grey
 
 
-def get_gradient(image_gray, kernel_size_row=2, kernel_size_col=2):
+def get_gradient(image_gray):
+    # get configs
+    global configs
+    kernel_size_row = configs['gradient']['kernel_size_row']
+    kernel_size_col = configs['gradient']['kernel_size_col']
     # make kernel matrix for dilation and erosion
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size_row, kernel_size_col))
+    # morph gradient
     image_gradient = cv2.morphologyEx(image_gray, cv2.MORPH_GRADIENT, kernel)
     return image_gradient
 
 
-def get_adaptive_gaussian_threshold(image_gray, block_size=30, subtract_val=30):
+def get_adaptive_gaussian_threshold(image_gray):
     """ Gray-scale 이 적용된 이미지를 입력받아서 Adaptive Threshold 를 적용한 흑백(Binary) 이미지객체를 반환합니다.
 
     :param image_gray: Gray-scale 이 적용된 이미지객체
-    :param block_size: 픽셀에 적용할 threshold 값을 계산하기 위한 블럭 크기(홀수). 적용될 픽셀이 블럭의 중심이 됨.
-    :param subtract_val: 보정 상수
     :return: Adaptive Threshold 를 적용한 흑백(Binary) 이미지객체
     """
-    # image_adaptive_gaussian = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, block_size, subtract_val)
+    # get configs
+    global configs
+    block_size = configs['threshold']['block_size']
+    subtract_val = configs['threshold']['subtract_val']
+    # adaptive threshold
     image_adaptive_gaussian = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, block_size, subtract_val)
     return image_adaptive_gaussian
 
 
-def get_adaptive_mean_threshold(image_gray, block_size=3, subtract_val=12):
+def get_adaptive_mean_threshold(image_gray):
     """ Gray-scale 이 적용된 이미지를 입력받아서 Adaptive Threshold 를 적용한 흑백(Binary) 이미지객체를 반환합니다.
-    @Params
-    * image_gray : Gray-scale 이 적용된 이미지객체
-    * block_size : 픽셀에 적용할   threshold 값을 계산하기 위한 블럭 크기(홀수). 적용될 픽셀이 블럭의 중심이 됨.
-    * subtract_val : 보정 상수
+    todo cv2.adaptiveThreshold() 의 세 번째 인자를 configuration file 을 통해 설정할 수 있도록 하고 메서드를 하나로 통일하기
     """
-    # image_adaptive_mean = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY,block_size, subtract_val)
+    # get configs
+    global configs
+    block_size = configs['threshold']['block_size']
+    subtract_val = configs['threshold']['subtract_val']
+    # adaptive threshold
     image_adaptive_mean = cv2.adaptiveThreshold(image_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, block_size, subtract_val)
     return image_adaptive_mean
 
@@ -111,22 +119,26 @@ def get_otsu_threshold(image_gray):
     return image_otsu
 
 
-def get_closing(image_gray, kernel_size_row=9, kernel_size_col=5):
+def get_closing(image_gray):
     """ Gray scale 이 적용된 이미지에 Morph Close 를 적용합니다.
     Closing : dilation 수행을 한 후 바로 erosion 수행을 하여 본래 이미지 크기로
     커널은 Image Transformation 을 결정하는 구조화된 요소
     커널의 크기가 크거나, 반복횟수가 많아지면 과하게 적용되어 경계가 없어질 수도 있다
     """
+    # get configs
+    global configs
+    kernel_size_row = configs['close']['kernel_size_row']
+    kernel_size_col = configs['close']['kernel_size_col']
     # make kernel matrix for dilation and erosion
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (kernel_size_row, kernel_size_col))
-    # closing 적용
+    # close
     image_close = cv2.morphologyEx(image_gray, cv2.MORPH_CLOSE, kernel)
     return image_close
 
 
 def get_contours(image_threshold):
     """ Threshold 가 적용된 이미지에 대하여 contour 를 추출하여 dictionary 형태로 반환합니다.
-    todo cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE 옵션도 인자로 받고 default 로 두기
+    todo cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE 옵션도 configuration 에서 설정할 수 있도록 하라
     """
     # contours는 point의 list형태.
     # hierarchy는 contours line의 계층 구조
@@ -137,23 +149,31 @@ def get_contours(image_threshold):
     return contours
 
 
-def remove_vertical_line(image, threshold=100, min_line_length=80, max_line_gap=5):
+def remove_vertical_line(image):
+    # get configs
+    global configs
+    threshold = configs['remove_line']['threshold']
+    min_line_length = configs['remove_line']['min_line_length']
+    max_line_gap = configs['remove_line']['max_line_gap']
+    # find lines
     lines = cv2.HoughLinesP(image, 1, np.pi / 180, threshold, min_line_length, max_line_gap)
     for line in lines:
-        x1, y1, x2, y2 = line[0]  # get line end point ( (x1, y1) , (x2, y2) )
+        x1, y1, x2, y2 = line[0]  # get end point of line : ( (x1, y1) , (x2, y2) )
         # remove line drawing black line
         cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
     return image
 
 
-def draw_contour_rect(image, contours, min_width=3, min_height=3):
+def draw_contour_rect(image, contours):
     """ 이미지위에 찾은 Contours 를 기반으로 외각사각형을 그리고 해당 이미지를 반환합니다.
     todo contour 의 색상이나 두깨도 인자로 받아서 설정할 수 있도록 변경하기
     """
-    # 찾은 contours 를 입력받은 이미지위에 그리기
-    # image_with_contours = cv2.drawContours(image, contours, -1, (0, 255, 0), 2)
+    # get configs
+    global configs
+    min_width = configs['contour']['min_width']
+    min_height = configs['contour']['min_height']
 
-    # Draw bounding rectangle
+    # Draw bounding rectangles
     for contour in contours:
         x, y, width, height = cv2.boundingRect(contour)  # 좌상단 꼭지점 좌표 , width, height
         # todo Rect 의 size 가 기준 이상인 것만 이미지 위에 그리기
@@ -167,7 +187,7 @@ def show_window(image, title):
     """ 윈도우를 열어서 이미지를 보여줍니다.
     """
     cv2.namedWindow(title, cv2.WINDOW_NORMAL)  # 사용자가 크기 조절할 수 있는 윈도우 생성
-    cv2.resizeWindow(title, 300, 500)
+    # cv2.resizeWindow(title, 300, 500)  todo 이미지 비율을 그대로 하면서 사이즈를 줄일 수 있도록 하라. getSize 찾아봐
     cv2.imshow(title, image)
     cv2.waitKey(0)  # 키보드 입력될 때 까지 계속 기다림
     cv2.destroyAllWindows()  # 이미지 윈도우 닫기
@@ -183,22 +203,7 @@ def process_image():
     """ 영향을 미치는 변수를 다양하게 적용해보면서 맞추어야 합니다.
     :return:
     """
-    block_size = 9 # Threshold (Odd number !!)
-    subtract_val = 12  # Threshold
-    gradient_kernel_size_row = 2  # Gradient Kernel Size
-    gradient_kernel_size_col = 2  # Gradient Kernel Size
-    close_kernel_size_row = 2  # Closing Kernel Size
-    close_kernel_size_col = 2  # Closing Kernel Size
-    min_width = 4  # Minimum Contour Rectangle Size
-    min_height = 10  # Minimum Contour Rectangle Size
-    threshold = 100  # Long Line Remove Precision
-    min_line_length = 100  # Long Line Remove  Minimum Line Length
-    max_line_gap = 5  # Long Line Remove Maximum Line Gap
-
-    # todo 위 경우 말고도 각각 메서드마다 적용되는 cv2.ABCDE 형식의 상수값도 모두 조정해줄 수 있어야한다.
-
     file_path = "images/car.png"
-    resulst_file_name = "block_s-" + str(block_size) + "subt-" + str(subtract_val) + ".png"
     image = open_original(file_path)
     # image = cv2.resize(image, (512, 712))
 
@@ -208,52 +213,46 @@ def process_image():
     save_image(image_gray, '_gray.png')  # save image as a file
 
     contours = get_contours(image_gray)
-    image_with_contours = draw_contour_rect(image, contours, min_width, min_height)
+    image_with_contours = draw_contour_rect(image, contours)
     show_window(image_with_contours, "result")  # show
 
     # Morph Gradient
-    image_gradient = get_gradient(image_gray, gradient_kernel_size_row, gradient_kernel_size_col)
+    image_gradient = get_gradient(image_gray)
     show_window(image_gradient, "image_gradient")  # show
     save_image(image_gradient, '_gradient.png')  # save image as a file
 
     contours = get_contours(image_gradient)
-    image_with_contours = draw_contour_rect(image, contours, min_width, min_height)
+    image_with_contours = draw_contour_rect(image, contours)
     show_window(image_with_contours, "result")  # show
 
     # Threshold
-    image_threshold = get_adaptive_mean_threshold(image_gradient, block_size, subtract_val)
+    image_threshold = get_adaptive_mean_threshold(image_gradient)
     show_window(image_threshold, "adaptive_threshold")  # show
     save_image(image_threshold, '_threshold.png')  # save image as a file
 
     contours = get_contours(image_threshold)
-    image_with_contours = draw_contour_rect(image, contours, min_width, min_height)
+    image_with_contours = draw_contour_rect(image, contours)
     show_window(image_with_contours, "result")  # show
 
     # Morph Close
-    image_close = get_closing(image_threshold, close_kernel_size_row, close_kernel_size_col)
+    image_close = get_closing(image_threshold)
     show_window(image_close, "image_close")  # show
     save_image(image_close, '_close.png')  # save image as a file
 
     contours = get_contours(image_close)
-    image_with_contours = draw_contour_rect(image, contours, min_width, min_height)
+    image_with_contours = draw_contour_rect(image, contours)
     show_window(image_with_contours, "result")  # show
 
     # Long line remove
-    remove_vertical_line(image_close, threshold, min_line_length, max_line_gap)
+    remove_vertical_line(image_close)
     show_window(image_close, "removed line")
     save_image(image_close, '_remove_line.png')  # save image as a file
 
     # Contours
     contours = get_contours(image_close)
-    image_with_contours = draw_contour_rect(image, contours, min_width, min_height)
+    image_with_contours = draw_contour_rect(image, contours)
     show_window(image_with_contours, "result")  # show
     save_image(image_with_contours, '_contour.png')  # save image as a file
-
-    # Gray
-    # Gradient
-    # Adaptive Threshold  (Binary Invert)
-    # Close
-    # Contours
 
     return None
 
@@ -261,6 +260,7 @@ def process_image():
 def main():
     read_configs('config.yml')
     print_configs()
+    process_image()
 
 
 if __name__ == "__main__":
