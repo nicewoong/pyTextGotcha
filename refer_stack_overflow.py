64471ad9815d20e369c6623024ef827ee947b332
nicewoong <thoughtscode@gmail.com>
2018-01-15 16:37:50 +0900

    Update reference code

--- refer_stack_overflow.py
+++ refer_stack_overflow.py
@@ -30,7 +30,9 @@ import numpy as np
 
 def detect_text(image_file):
     large = cv2.imread(image_file)
-    # rgb = cv2.pyrDown(large)
+    # down-sample and use it for processing
+    large = cv2.pyrDown(large)
+    # large = cv2.pyrUp(large)
     small = cv2.cvtColor(large, cv2.COLOR_BGR2GRAY)
 
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
@@ -43,33 +45,46 @@ def detect_text(image_file):
     # using RETR_EXTERNAL instead of RETR_CCOMP
     _, contours, hierarchy = cv2.findContours(connected.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-    mask = np.zeros(bw.shape, dtype=np.uint8)
+    draw_contours(bw, contours, large)
+
+    # cv2.imshow('rects', rgb)
+    show_window(large, 'result')
+
+
+def draw_contours(image_threshold, contours, image_origin):
+    mask = np.zeros(image_threshold.shape, dtype=np.uint8)
 
     for idx in range(len(contours)):
         x, y, w, h = cv2.boundingRect(contours[idx])
-        mask[y:y+h, x:x+w] = 0
+        # fill the contour
+        # fills the area bounded by the contours if thickness < 0.
+        # It's done so we can check the ratio of non-zero pixels to decide if the box likely contains text
+        mask[y:y + h, x:x + w] = 0
         cv2.drawContours(mask, contours, idx, (255, 255, 255), -1)
-        r = float(cv2.countNonZero(mask[y:y+h, x:x+w])) / (w * h)
+        r = float(cv2.countNonZero(mask[y:y + h, x:x + w])) / (w * h)
 
         if r > 0.45 and w > 8 and h > 8:
-            cv2.rectangle(large, (x, y), (x+w-1, y+h-1), (0, 255, 0), 2)
-
-    # cv2.imshow('rects', rgb)
-    show_window(large, 'reulst')
+            cv2.rectangle(image_origin, (x, y), (x + w - 1, y + h - 1), (0, 255, 0), 2)
 
 
 def show_window(image, title):
     """ 윈도우를 열어서 이미지를 보여줍니다.
     """
-    cv2.namedWindow(title, cv2.WINDOW_NORMAL)  # 사용자가 크기 조절할 수 있는 윈도우 생성
-    # cv2.resizeWindow(title, 300, 500)
-    cv2.imshow(title, image)
-    cv2.waitKey(0)  # 키보드 입력될 때 까지 계속 기다림
-    cv2.destroyAllWindows()  # 이미지 윈도우 닫기
+    height, width = image.shape[:2]  # get image size
+    if height > 500:
+        rate = 500 / height
+        height = round(height * rate)
+        width = round(width * rate)
+
+    cv2.namedWindow(title, cv2.WINDOW_NORMAL)  # Create a window that the user can resize
+    cv2.resizeWindow(title, width, height)  # resize window according to the size of the image
+    cv2.imshow(title, image)  # open image window
+    cv2.waitKey(0)  # Continue to wait until keyboard input
+    cv2.destroyAllWindows()
 
 
 def main():
-    detect_text('images/test1.jpg')
+    detect_text('images/test_0.jpg')
 
 
 if __name__ == "__main__":
