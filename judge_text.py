7a8d8d1df2c319cbe520ac0ef487c27234acda25
nicewoong <thoughtscode@gmail.com>
2018-01-23 17:21:56 +0900

    Create judge_text.py

---
+++ judge_text.py
@@ -0,0 +1,53 @@
+
+import numpy as np
+import tensorflow as tf
+
+import sys
+
+
+imagePath = sys.argv[1]
+modelFullPath = 'C:/Users/viva/PycharmProjects/images_cropped/retrained_graph.pd'
+labelsFullPath = 'C:/Users/viva/PycharmProjects/images_cropped/retrained_labels.txt'
+
+
+def create_graph():
+    with tf.gfile.FastGFile(modelFullPath, 'rb') as f:
+        graph_def = tf.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tf.import_graph_def(graph_def, name='')
+
+
+def run_inference_on_image():
+    answer = None
+
+    if not tf.gfile.Exists(imagePath):
+        tf.logging.fatal('File does not exist %s', imagePath)
+        return answer
+
+    image_data = tf.gfile.FastGFile(imagePath, 'rb').read()
+
+
+    create_graph()
+
+    with tf.Session() as sess:
+
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        predictions = sess.run(softmax_tensor,
+                               {'DecodeJpeg/contents:0': image_data})
+        predictions = np.squeeze(predictions)
+
+        top_k = predictions.argsort()[-5:][::-1]
+        f = open(labelsFullPath, 'rb')
+        lines = f.readlines()
+        labels = [str(w).replace("\n", "") for w in lines]
+        for node_id in top_k:
+            human_string = labels[node_id]
+            score = predictions[node_id]
+            print('%s (score = %.5f)' % (human_string, score))
+
+        answer = labels[top_k[0]]
+        return answer
+
+
+if __name__ == '__main__':
+    run_inference_on_image()
